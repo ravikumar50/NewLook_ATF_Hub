61c0a86b915cb7fc25cf4f9c35b992658634de2b
Kumar <ravi.fu.kumar@accenture.com>
2025-06-06 02:15:08 +0530

    Added the home page

--- ATF_Hub_FrontEnd/src/App.jsx
+++ ATF_Hub_FrontEnd/src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './Components/Footer'
-import HomeLayout from './Layouts/Homelayout'
 import Login from './Pages/Login'
+import HomePage from './Pages/HomePage'
 
 function App() {
   return(
     <>
       <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
       </Routes>
     </>
