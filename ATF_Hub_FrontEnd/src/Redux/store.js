fdcde0d6fd6cbbc33ca08c0e143ece8034aaffb1
Kumar <ravi.fu.kumar@accenture.com>
2025-06-06 02:10:49 +0530

    Added the login Page

--- ATF_Hub_FrontEnd/src/Redux/store.js
+++ ATF_Hub_FrontEnd/src/Redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./Slices/AuthSlice";
+
 const store = configureStore({
     reducer : {
+        auth : authSliceReducer,
     },
     devTools: true,
 })
