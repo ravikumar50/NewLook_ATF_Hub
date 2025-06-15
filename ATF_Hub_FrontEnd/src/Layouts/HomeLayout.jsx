b19c66a4700180e6849aa62380f361a56eb2e212
Kumar <ravi.fu.kumar@accenture.com>
2025-06-16 02:08:50 +0530

    Nothing changed

--- ATF_Hub_FrontEnd/src/Layouts/HomeLayout.jsx
+++ ATF_Hub_FrontEnd/src/Layouts/HomeLayout.jsx
@@ -99,7 +99,7 @@ function HomeLayout({children}){
                             <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
                                 <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-full text-center" onClick={() => navigate("/")}>
-                                    Login
+                                    Logout
                                 </button>
                                 </div>
                             </li>
